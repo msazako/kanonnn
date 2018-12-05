91f8ba4ccb82e1b853be9ec4dc747585b253ac9a
msazako <31146216+msazako@users.noreply.github.com>
2018-12-04 20:48:19 -0600

    Update neko.js

--- neko.js
+++ neko.js
@@ -31,4 +31,4 @@ client.on("message", message => {
 });
 
 // -- Program Entry Point (Login) ------------------------------------------------------------------
-client.login(process.env.BOT_T0KEN);
+client.login(process.env.B0T_T0KEN);
