12f02350c1353b8cb9ce4735562d121fa61e8494
msazako <31146216+msazako@users.noreply.github.com>
2018-12-04 21:15:18 -0600

    Update neko.js

--- neko.js
+++ neko.js
@@ -1,33 +1,15 @@
-"use strict"; // Required by Node for ES6
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const Discord = require("discord.js");
-
-const commands = require(__dirname + "/src/commands.js");
-const version  = require(__dirname + "/src/version.js");
-
-var client = new Discord.Client({ "maxCachedMessages" : 200, "revive" : true });
-
-// =================================================================================================
-// Initialization
-// =================================================================================================
-
-client.once("ready", () => { client.setPlayingGame(config.game || `Neko.js ${version.current}`) });
-
-// =================================================================================================
-// Command Reciever
-// =================================================================================================
-
-client.on("message", message => {
-
-    if (message.author.equals(client.user)) return; // prevent bot from gaining sentience
-
-    // -- Command Parsers --------------------------------------------------------------------------
-    //let command = admin.parse(client, message, config);
-    let command = commands.parse(client, message, config);
-    //command = music.parse(client, message, config);
-    if (command) command.execute(client, message, config);
-
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-// -- Program Entry Point (Login) ------------------------------------------------------------------
-client.login(process.env.B0T_T0KEN);
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
