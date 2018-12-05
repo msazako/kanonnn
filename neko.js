ed76f7f118a95a28ff697c65a5b97cef7e0c5484
msazako <31146216+msazako@users.noreply.github.com>
2018-12-04 22:05:38 -0600

    Update neko.js

--- neko.js
+++ neko.js
@@ -1,16 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.on('ready', () => {
-    // Set bot status to: "Playing with JavaScript"
-    client.user.setActivity("with your loved ones' carcasses")
-
-    // Alternatively, you can set the activity to any of the following:
-    // PLAYING, STREAMING, LISTENING, WATCHING
-    // For example:
-    // client.user.setActivity("TV", {type: "WATCHING"})
-})
-
 client.on('message', (receivedMessage) => {
     if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
         return
