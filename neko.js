cf40d450ec8cd9ebc2dd50e3bfcf8490549d6ffa
msazako <31146216+msazako@users.noreply.github.com>
2018-12-04 21:48:08 -0600

    Update neko.js

--- neko.js
+++ neko.js
@@ -1,33 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const commands = require(__dirname + "/src/commands.js");
-const version  = require(__dirname + "/src/version.js");
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
-
-
-var client = new Discord.Client({ "maxCachedMessages" : 200, "revive" : true });
-
-// =================================================================================================
-// Initialization
-// =================================================================================================
-
-client.once("ready", () => { client.setPlayingGame(config.game || `Neko.js ${version.current}`) });
-
-// =================================================================================================
-// Command Reciever
-// =================================================================================================
-
-client.on("message", message => {
-
-    if (message.author.equals(client.user)) return; // prevent bot from gaining sentience
-
-    // -- Command Parsers --------------------------------------------------------------------------
-    //let command = admin.parse(client, message, config);
-    let command = commands.parse(client, message, config);
-    //command = music.parse(client, message, config);
-    if (command) command.execute(client, message, config);
-
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
