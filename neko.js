0231c7008c3352eaf20c3e1ae484f939056a7fe9
msazako <31146216+msazako@users.noreply.github.com>
2018-12-04 21:35:41 -0600

    Update neko.js

--- neko.js
+++ neko.js
@@ -1,20 +1,5 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const commands = require(__dirname + "/src/commands.js");
-const version  = require(__dirname + "/src/version.js");
-
-
-var client = new Discord.Client({ "maxCachedMessages" : 200, "revive" : true });
-
-// =================================================================================================
-// Initialization
-// =================================================================================================
-
-client.once("ready", () => { client.setPlayingGame(config.game || `Neko.js ${version.current}`) });
-
-// =================================================================================================
-// Command Reciever
-// =================================================================================================
 
 client.on('ready', () => {
     console.log('I am ready!');
